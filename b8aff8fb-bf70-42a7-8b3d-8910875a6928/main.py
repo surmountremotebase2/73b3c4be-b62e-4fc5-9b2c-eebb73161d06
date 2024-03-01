5877d4ca420cb15d79f752813b75f1a173fb89a4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-01 11:36:39 -0500

    User Vivek Singh saved changes to main.py at 03/01/2024, 16:36:38 UTC time.

--- b8aff8fb-bf70-42a7-8b3d-8910875a6928/main.py
+++ b8aff8fb-bf70-42a7-8b3d-8910875a6928/main.py
@@ -15,7 +15,7 @@ class TradingStrategy(Strategy):
     def interval(self):
         # Define the interval for which the strategy will be applied. 
         # This is not used in this simple allocation but required by the framework.
-        return "30day"
+        return "28day"
 
     def run(self, data):
         # Define an equal allocation dictionary for Apple and Microsoft
